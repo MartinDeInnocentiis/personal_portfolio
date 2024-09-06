11cad898d969889a2cdbfe127fd40602899997a2
Martin De Innocentiis <martindeinnocentiis90@gmail.com>
2024-09-06 20:29:49 -0300

    Working on the display of the project detail card

--- frontend/src/screens/ProjectDetailScreen/ProjectDetailScreen.jsx
+++ frontend/src/screens/ProjectDetailScreen/ProjectDetailScreen.jsx
@@ -11,6 +11,8 @@ const ProjectDetailScreen = () => {
     const commentInputRef = useRef(null);
     const { user } = useAuthStore();
 
+    const [isExpanded, setIsExpanded] = useState(false);
+
     const scrollToCommentInput = () => {
         if (commentInputRef.current) {
             commentInputRef.current.scrollIntoView({ behavior: 'smooth' });
@@ -33,29 +35,29 @@ const ProjectDetailScreen = () => {
 
             const storedLikeId = localStorage.getItem('likeId');
             const storedHeartId = localStorage.getItem('heartId');
-    
+
             if (storedLikeId) {
                 setLikeId(storedLikeId);
                 setLiked(true);
             }
-    
+
             if (storedHeartId) {
                 setHeartId(storedHeartId);
                 setHearted(true);
             }
-    
+
             const { data } = await api.get(`/posts/${id}/`);
             setProjectDetail(data);
             setTotalLikes(data.total_likes);
             setTotalHearts(data.total_hearts);
             setTotalComments(data.total_comments);
-    
+
             const userLike = data.likes.find(like => (user && like.user === user.id) || (user && user.anon_user_id && like.anon_user === user.anon_user_id));
             if (userLike) {
                 setLikeId(userLike.id);
                 localStorage.setItem('likeId', userLike.id);
             }
-    
+
             const userHeart = data.hearts.find(heart => (user && heart.user === user.id) || (user && user.anon_user_id && heart.anon_user === user.anon_user_id));
             if (userHeart) {
                 setHeartId(userHeart.id);
@@ -65,52 +67,19 @@ const ProjectDetailScreen = () => {
         fetchData();
     }, [id, user]);
 
-/*   useEffect(() => {
-        async function fetchData() {
-            const { data } = await api.get(`/posts/${id}/`);
-            setProjectDetail(data);
-            setLiked(data.user_has_liked);
-            setHearted(data.user_has_hearted);
-            setTotalLikes(data.total_likes);
-            setTotalHearts(data.total_hearts);
-            setTotalComments(data.total_comments);
+    // Verificamos si projectDetail está disponible
+    if (!projectDetail) {
+        return <div>Loading...</div>;
+    }
 
-            setLikeId(data.like_id);
-            setHeartId(data.heart_id);
+    // Mapeamos las tecnologías con #
+    const stackWithHashtags = projectDetail.stack
+        ? projectDetail.stack.split(', ').map((tech) => `#${tech}`)
+        : [];
 
-            // FIND LIKE & HEART IDs IF THEY EXIST
-            const userLike = data.likes.find(like => (user && like.user === user.id) || (user && user.anon_user_id && like.anon_user === user.anon_user_id));
-            if (userLike) {
-                setLikeId(userLike.id);
-            }
-
-            const userHeart = data.hearts.find(heart => (user && heart.user === user.id) || (user && user.anon_user_id && heart.anon_user === user.anon_user_id));
-            if (userHeart) {
-                setHeartId(userHeart.id);
-            }
-        }
-        fetchData();
-    }, [id, user]);*/
-
-
-    /*const handleLike = async () => {
-        try {
-            if (!liked) {
-                const response = await api.post(`/posts/${id}/likes/`);
-                setLiked(true);
-                const likeId = parseInt(response.data.id); // Asegúrate de que response.data.id es un número
-                setLikeId(likeId);
-                setTotalLikes(prev => prev + 1);  // Incrementa el conteo de likes
-            } else {
-                await api.delete(`/posts/likes/${likeId}/`);
-                setLiked(false);
-                setLikeId(null);
-                setTotalLikes(prev => prev - 1);  // Decrementa el conteo de likes
-            }
-        } catch (error) {
-            console.error('Error handling like:', error);
-        }
-    };*/
+    const toggleExpand = () => {
+        setIsExpanded(prev => !prev);
+    };
 
 
 
@@ -169,28 +138,6 @@ const ProjectDetailScreen = () => {
         }
     };
 
-    /*const handleHeart = async () => {
-        try {
-            if (!hearted) {
-                const response = await api.post(`/posts/${id}/hearts/`);
-                setHearted(true);
-                console.log('Post HEART Response:', response);
-                const heartId = parseInt(response.data.id);
-                setHeartId(heartId);
-                setTotalHearts(prev => prev + 1);
-            } else {
-                const response = await api.delete(`/posts/hearts/${heartId}/`);
-                setHearted(false);
-                console.log('Delete HEART Response:', response);
-                setHeartId(null);
-                setTotalHearts(prev => prev - 1);
-            }
-            setHearted(!hearted);
-        } catch (error) {
-            console.error('Error handling heart:', error);
-        }
-    };*/
-
 
 
 
@@ -210,11 +157,15 @@ const ProjectDetailScreen = () => {
     return (
         <div className='project-detail-container'>
             <Link to="/projects" className="back-button">Back</Link>
+
+
             <div className="project-detail-card">
                 <img src={projectDetail.image} alt={projectDetail.title} className="project-detail-image" />
                 <div className="project-detail-content">
                     <h2 className="project-detail-title">{projectDetail.title}</h2>
                     <p className="project-detail-description">{projectDetail.description}</p>
+
+
                     <div className="project-detail-reactions">
                         <span className="project-like" onClick={handleLike}>
                             {liked ? <BiSolidLike className='reaction-icon-like-solid' /> : <BiLike className='reaction-icon-like' />} <div> {totalLikes}</div>
@@ -229,6 +180,33 @@ const ProjectDetailScreen = () => {
                     <div className="project-detail-extra-info">
                         {/* Otra información extra del proyecto */}
                     </div>
+
+                    
+                    <div className='expand-button-container'>
+                        <button onClick={toggleExpand} className={`expand-button ${isExpanded ? 'expanded' : ''}`}>
+                            <span className='span-button-detail'> {isExpanded ? 'Hide details' : 'View details'}</span>
+                        </button>
+                    </div>
+
+                    <div className={`stack-container ${isExpanded ? 'expanded' : 'collapsed'}`}>
+                        <div className='stack-container2'>
+                            <p className='details-p'>Website: <span className='website-link'>{projectDetail.website_link}</span></p>
+                            <p className='details-p'>GitHub: <span className='github-link'>{projectDetail.github_link}</span></p>
+                            <p className='details-p'>Status: <span className='status'>{projectDetail.status}</span></p>
+                            <hr className='hr-details' />
+                            <div className="stack-tags">
+                                <span className='stack-title'>Tech Stack:</span>
+                                <div className="stack-grid">
+                                    {stackWithHashtags.map((hashtag, index) => (
+                                        <span key={index} className="stack-hashtag">
+                                            {hashtag}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
             <div>
